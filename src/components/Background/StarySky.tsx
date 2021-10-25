4317b50fdd97c808d24c3a9f6a0df821b93a0cb1
Joseph Walker <Joseph.Walker@softwire.com>
2021-10-25 19:46:17 +0100

    Added empty background component

---
+++ src/components/Background/StarySky.tsx
@@ -0,0 +1,19 @@
+import React, { ReactElement } from 'react';
+
+export const StarySky: React.FC = () =>
+{
+    return (
+        <div className='stary-sky-background'>
+            <h1>Temporary information</h1>
+        </div>
+    );
+}
+
+// interface TestFuncProps {
+//     name: string;
+//     age: number;
+// }
+
+// const TestFunc: React.FC<TestFuncProps> = (props) => {
+//     return (<div></div>)
+// }
