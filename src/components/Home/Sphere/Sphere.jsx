9276a197e1c3ff3ecb14f3debd6e8357c20eb10b
Jordan <jordan.d.carlton@gmail.com>
2021-10-25 19:44:04 +0100

    add sphere to App

---
+++ src/components/Home/Sphere/Sphere.jsx
@@ -0,0 +1,43 @@
+import React, { useRef, Suspense } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import {Canvas, useFrame, useLoader} from "react-three-fiber";
+import model from '../../../assets/Sphere/mars.glb'
+import CameraControls from "./CameraControls";
+import './styles.scss'
+
+const Mars = () => {
+    const planet = useRef();
+
+    const { nodes } = useLoader(GLTFLoader, model);
+    // model is from https://solarsystem.nasa.gov/resources/2372/mars-3d-model/
+    // use GTLFLoader from three.js and useLoader hook from react-three-fiber to load model
+
+    useFrame(() => (planet.current.rotation.y += 0.0002));
+    // Adds rotation to planet
+
+    return (
+        <mesh
+            ref={planet}
+            visible
+            position={[0, 0, 0]}
+            // Adding data from mars.glb to the geometry and material of the sphere
+            geometry={nodes.Cube008.geometry}
+            material={nodes.Cube008.material}
+        />
+    );
+};
+
+const Sphere = () => {
+    return (
+        <Canvas className="canvas">
+            <CameraControls />
+            <directionalLight intensity={1} />
+            <ambientLight intensity={0.6} />
+            <Suspense fallback="loading">
+                <Mars />
+            </Suspense>
+        </Canvas>
+    )
+}
+
+export default Sphere;
