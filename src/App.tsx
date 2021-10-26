263a2cb72cbe48c6d0036042ad852c0782bb8b36
Jordan <jordan.d.carlton@gmail.com>
2021-10-26 12:30:58 +0100

    Initial NavBar commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,30 @@ import { HomePage } from './pages/Home';
 import { GalleryPage } from './pages/Gallery';
 import { RovGuessr } from './pages/RovGuessr';
 import { RoverDetails } from './pages/RoverDetails';
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <Router>
-        {/* Components that are on all pages go outside of switch */}
-        <Switch>
-            <Route path="">
-                <HomePage/>
-            </Route>
-            <Route path="/gallery">
-                <GalleryPage/>
-            </Route>
-            <Route path="/RovGuessr">
-                <RovGuessr/>
-            </Route>
-            <Route path="/roverDetails">
-                <RoverDetails/>
-            </Route>
-        </Switch>
-    </Router>
+      <div className="App">
+        <NavBar />
+        <Router>
+            {/* Components that are on all pages go outside of switch */}
+            <Switch>
+                <Route path="">
+                    <HomePage/>
+                </Route>
+                <Route path="/gallery">
+                    <GalleryPage/>
+                </Route>
+                <Route path="/RovGuessr">
+                    <RovGuessr/>
+                </Route>
+                <Route path="/roverDetails">
+                    <RoverDetails/>
+                </Route>
+            </Switch>
+        </Router>
+    </div>
   );
 }
 
