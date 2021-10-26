a83a98593e1f1efa5da2912baa1cb87024dcbd8d
Joseph Walker <Joseph.Walker@softwire.com>
2021-10-26 12:34:26 +0100

    Added stars to app and positioned them randomly on the screen

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import { HomePage } from './pages/Home';
 import { GalleryPage } from './pages/Gallery';
 import { RovGuessr } from './pages/RovGuessr';
 import { RoverDetails } from './pages/RoverDetails';
+import { StarySky } from './components/Background/StarySky';
 
 function App() {
   return (
     <Router>
         {/* Components that are on all pages go outside of switch */}
+        <StarySky numberOfStars={100}/>
         <Switch>
             <Route path="">
                 <HomePage/>
--- src/components/Background/StarySky.tsx
+++ src/components/Background/StarySky.tsx
@@ -1,19 +1,50 @@
 import React, { ReactElement } from 'react';
+import './StarySky.scss'
 
-export const StarySky: React.FC = () =>
+interface StarySkyProps
 {
+    numberOfStars: number;
+
+    imagesForStars?: SingleStarProp;
+}
+
+export const StarySky: React.FC<StarySkyProps> = (props: StarySkyProps) =>
+{
+    const allStars: Array<ReactElement> = [];
+
+    for (let i = 0; i < props.numberOfStars; i++)
+    {
+        allStars.push(<SingleStar urls={props.imagesForStars?.urls}/>)
+    }
+
     return (
         <div className='stary-sky-background'>
-            <h1>Temporary information</h1>
+            {allStars}
         </div>
     );
 }
 
-// interface TestFuncProps {
-//     name: string;
-//     age: number;
-// }
+interface SingleStarProp
+{
+    // List of image urls to use for random stars to give variety
+    // If unused, then just use point light
+    urls?: Array<string>;
+}
 
-// const TestFunc: React.FC<TestFuncProps> = (props) => {
-//     return (<div></div>)
-// }
+const SingleStar: React.FC<SingleStarProp> = ({urls = []}: SingleStarProp) =>
+{
+    if (urls.length === 0)
+    {
+        // Create small elements with css styling for the stars
+        const randomPositionX = Math.floor(Math.random() * window.screen.width);
+        const randomPositionY = Math.floor(Math.random() * window.screen.height);
+
+        return <div className='single-star' style={{top: randomPositionY, left: randomPositionX}}></div>
+    }
+    else
+    {
+        const chosenURL = urls[Math.floor(Math.random() * urls.length)];
+
+        return <img src={chosenURL}/>
+    }
+}
