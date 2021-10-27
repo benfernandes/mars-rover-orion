02c7b65cc6037051711e987642be560b4855b912
Joseph Walker <Joseph.Walker@softwire.com>
2021-10-27 12:02:46 +0100

    Able to retrieve current position of perseverance rover

---
+++ src/APIs/RoverPositionRepo.ts
@@ -0,0 +1,25 @@
+export enum Mission
+{
+    Perseverance = "M20",
+}
+
+export interface RoverPosition
+{
+    features: {
+        properties: {
+            lat: number;
+            lon: number;
+        };
+    }[];
+}
+
+export class RoverPositionRepo
+{    
+    static async GetRoverPosition(mission: Mission)
+    {
+        const response = await fetch("https://mars.nasa.gov/mmgis-maps/M20/Layers/json/M20_waypoints_current.json");
+        const json = await response.json();
+
+        return (await <RoverPosition>json).features[0].properties;
+    }
+}
