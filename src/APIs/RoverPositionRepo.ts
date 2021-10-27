203072af29081a0f5b55fb01c55eb571e2eb05c9
Joseph Walker <Joseph.Walker@softwire.com>
2021-10-27 12:07:25 +0100

    Ability to choose what rover to get. Only perseverance currently

--- src/APIs/RoverPositionRepo.ts
+++ src/APIs/RoverPositionRepo.ts
@@ -17,7 +17,7 @@ export class RoverPositionRepo
 {    
     static async GetRoverPosition(mission: Mission)
     {
-        const response = await fetch("https://mars.nasa.gov/mmgis-maps/M20/Layers/json/M20_waypoints_current.json");
+        const response = await fetch(`https://mars.nasa.gov/mmgis-maps/M20/Layers/json/${mission}_waypoints_current.json`);
         const json = await response.json();
 
         return (await <RoverPosition>json).features[0].properties;
