524236925f54cfc04f37507ddb29f4db4797264e
Brighid <brighid.mcneff@softwire.com>
2021-10-28 10:03:44 +0100

    API call for rover pics

--- src/APIs/RoverPhotoRepo.ts
+++ src/APIs/RoverPhotoRepo.ts
@@ -10,11 +10,9 @@ export interface RoverPhotos {
     }>,
 }
 
-
 export async function GetRoverPhotos(rover: Rover, sol = 1, camera?: string) {
     const response = await fetch(camera == undefined ? `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&api_key=${process.env.REACT_APP_NASA_API_KEY}` : `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&camera=${camera}&api_key=${process.env.REACT_APP_NASA_API_KEY}`)
     const json = await response.json();
 
     return (json as RoverPhotos).photos;
 }
-
