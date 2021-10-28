c06dca424c1f89b149bcec9420def26478f7c8ec
Jordan <jordan.d.carlton@gmail.com>
2021-10-28 18:41:46 +0100

    Add pagination

--- src/APIs/RoverPhotoRepo.ts
+++ src/APIs/RoverPhotoRepo.ts
@@ -12,7 +12,7 @@ export interface RoverPhotos {
 
 export async function GetRoverPhotos(rover: Rover, sol = 1, camera?: string) {
     try {
-        const response = await fetch(camera == undefined ? `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&page=1&api_key=${process.env.REACT_APP_NASA_API_KEY}` : `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&camera=${camera}&api_key=${process.env.REACT_APP_NASA_API_KEY}`)
+        const response = await fetch(camera === undefined ? `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&page=1&api_key=${process.env.REACT_APP_NASA_API_KEY}` : `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&page=1&camera=${camera}&api_key=${process.env.REACT_APP_NASA_API_KEY}`)
         const json = await response.json();
         return (json as RoverPhotos).photos;
 
