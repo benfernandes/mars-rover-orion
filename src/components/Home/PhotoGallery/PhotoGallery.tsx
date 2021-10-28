4e8321a6174a3100029fc7cf0f6a507bf16c0ec7
Besar <besar.krasniqi@softwire.com>
2021-10-28 13:47:55 +0100

    Added in function to get api working with photoviewer

--- src/components/Home/PhotoGallery/PhotoGallery.tsx
+++ src/components/Home/PhotoGallery/PhotoGallery.tsx
@@ -1,8 +1,21 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './styles.scss'
+import {GetRoverPhotos} from "../../../APIs/RoverPhotoRepo";
+import {GetRoverManifest, Rover} from "../../../APIs/RoverManifest";
 
 
 const PhotoGallery = ({ slides }: {slides: Array<{image:string}>}) => {
+    const [imageData, setImageData] = useState<string[]>([]);
+    GetRoverManifest(Rover.spirit).then()
+
+useEffect(() => {
+    GetRoverPhotos(Rover.curiosity, 0, "FHAZ").then(data => {
+        setImageData(data.map(image => image.img_src))
+        console.log(data)
+    })
+})
+
+
 
     const [current, setCurrent] = useState(0);
     const length = slides.length
@@ -15,14 +28,15 @@ const PhotoGallery = ({ slides }: {slides: Array<{image:string}>}) => {
         setCurrent(current === 0 ? length -1 : current - 1)
     }
 
+
     return (
         <div className="SliderImages">
             <img src={"/icons/left.svg"} className="left-arrow" alt="Left Arrow" onClick={prevSlide}/>
             <img src={"/icons/right.svg"} className="right-arrow" alt="Right Arrow" onClick={nextSlide}/>
-            {slides.map((images, index) => {
+            {imageData.map((images, index) => {
                 return (
                     <div className={index === current ? "slide-active" : "slide"} key={index}>
-                        {index === current && (<img src={images.image} alt="x" className="singleImage"/>)}
+                        {index === current && (<img src={images} alt="x" className="singleImage"/>)}
                     </div>
                 )
             })}
