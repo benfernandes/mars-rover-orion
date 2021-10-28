2952de925d944eae449296fc2f476048ce387648
Brighid <brighid.mcneff@softwire.com>
2021-10-28 09:55:21 +0100

    updated with requested changes

---
+++ src/APIs/RoverManifest.ts
@@ -0,0 +1,27 @@
+export enum Rover {
+    perseverance = "perseverance",
+    curiosity = "curiosity",
+    spirit = "spirit",
+}
+
+export interface RoverManifest {
+    photo_manifest: {
+        launch_date: string,
+        landing_date: string,
+        status: string,
+        max_sol: number,
+        total_photos: number,
+        photos: Array<{
+            sol: number,
+            total_photos: number,
+            cameras: string[],
+        }>,
+    }
+}
+
+export async function GetRoverManifest(rover: Rover) {
+    const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${process.env.REACT_APP_NASA_API_KEY}`); // wither api key?
+    const json = await response.json();
+
+    return (json as RoverManifest).photo_manifest;
+}
