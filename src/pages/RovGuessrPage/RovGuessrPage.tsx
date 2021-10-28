0ee5a3f25468751068ebdde2953d28b8e2859cbb
Joseph Walker <Joseph.Walker@softwire.com>
2021-10-28 12:23:13 +0100

    Added small tweaks to design

--- src/pages/RovGuessrPage/RovGuessrPage.tsx
+++ src/pages/RovGuessrPage/RovGuessrPage.tsx
@@ -3,6 +3,8 @@ import './styles.scss'
 
 const RovGuessrPage = () => {
     return <div className='rov-guessr-page'>
+        <h1>RovGuessr™</h1>
+        <p>Guess what rover this picture was taken from</p>
         <img src="https://media-exp1.licdn.com/dms/image/C4D03AQF7C5KzdUFLcg/profile-displayphoto-shrink_800_800/0/1562106459334?e=1639612800&v=beta&t=7NqH2EZyTry0de4o9_1UqV5YVi_GOV6smcvpXiDCSb4"/>
         <ul className='rover-selection'>
             <li><button>Curiosity</button></li>
