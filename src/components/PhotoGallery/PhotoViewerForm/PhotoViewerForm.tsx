3877d8e1ac94b300b1c9560a7cff7b1a91d361e7
Jordan <jordan.d.carlton@gmail.com>
2021-10-28 14:56:11 +0100

    Add numeric input to form

--- src/components/PhotoGallery/PhotoViewerForm/PhotoViewerForm.tsx
+++ src/components/PhotoGallery/PhotoViewerForm/PhotoViewerForm.tsx
@@ -1,24 +1,71 @@
 import {useState} from "react";
 import './PhotoViewerForm.scss';
 import {Rover} from "../../../APIs/RoverManifest";
+import NumericInput from 'react-numeric-input';
 
 //TODO remove props : any
 const PhotoViewerForm = (props : any) => {
 
     const [inputs, setInputs] = useState({})
+    const [selectedOption, setSelectedOption] = useState(null)
+    const [selectedDay, setSelectedDay] = useState(0)
 
     //TODO remove event : any
     const handleSubmit = (event : any) => {
 
     }
 
-    // GetRoverPhotos(rover: Rover, sol = 1, camera?: string)
+    const onValueChange = (event : any) => {
+        setSelectedOption(event.target.value)
+    }
+
+    const handleDayChange = (event : any) => {
+        setSelectedDay(event)
+        console.log(selectedDay)
+    }
+
+    // GetRoverPhotos(sol = 1, camera?: string)
     return (
         <div className="photo-viewer-form">
             <form onSubmit={handleSubmit}>
                 <fieldset>
                     <legend>Select which photos you'd like to see</legend>
-
+                    <div className="form-radio-container">
+                        <label>
+                            <input
+                                type="radio"
+                                value="Perseverance"
+                                checked={selectedOption === "Perseverance"}
+                                onChange={onValueChange}
+                            />
+                            Perseverance
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                value="Curiosity"
+                                checked={selectedOption === "Curiosity"}
+                                onChange={onValueChange}
+                            />
+                            Curiosity
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                value="Spirit"
+                                checked={selectedOption === "Spirit"}
+                                onChange={onValueChange}
+                            />
+                            Spirit
+                        </label>
+                    </div>
+                    <label htmlFor="selectSol">Select a day:</label>
+                    {/*TODO set max to be max from selected rover*/}
+                    <NumericInput
+                        min={0}
+                        // max={100}
+                        onChange={handleDayChange}
+                    />
                 </fieldset>
             </form>
 
