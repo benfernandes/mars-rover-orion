6859e4109c02a6560d75dab5ae1ecead17405471
Joseph Walker <Joseph.Walker@softwire.com>
2021-10-28 10:56:36 +0100

    Refreshing cached images using other API

--- src/APIs/RovGuessrGalleryRepo.ts
+++ src/APIs/RovGuessrGalleryRepo.ts
@@ -1,4 +1,5 @@
-import { Rover } from "./RoverManifest";
+import { GetRoverManifest, Rover } from "./RoverManifest";
+import { GetRoverPhotos } from "./RoverPhotoRepo";
 
 let cachedImages: {
     "curiosity": string[],
@@ -18,5 +19,10 @@ export async function getRandomImage(rover: Rover)
 
 async function refreshCachedImages(rover: Rover)
 {
-    
+    const manifest = await GetRoverManifest(rover);
+
+    const randomSol = Math.floor(Math.random() * manifest.max_sol);
+
+    const roverPhotos = await GetRoverPhotos(rover, randomSol, "FHAZ")
+    cachedImages[rover] = roverPhotos.map(photo => photo.img_src)
 }
