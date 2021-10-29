f311ea14eea47a465592343f458c2c195e799305
Joseph Walker <Joseph.Walker@softwire.com>
2021-10-29 10:27:27 +0100

    Fixed PR comments to clean up code and keep getting images until we find some

--- src/APIs/RovGuessrGalleryRepo.ts
+++ src/APIs/RovGuessrGalleryRepo.ts
@@ -1,35 +1,42 @@
 import { GetRoverManifest, Rover } from "./RoverManifest";
 import { GetRoverPhotos } from "./RoverPhotoRepo";
 
-var cachedImages: {
+interface ImageUrlCache {
     curiosity: string[],
     perseverance: string[],
     spirit: string[],
-};
+}
 
-export async function getRandomImage(rover: Rover, refreshImages: boolean = false)
-{
-    if (!cachedImages)
-        cachedImages = {
+let cachedImageUrls: ImageUrlCache;
+
+export async function getRandomImage(rover: Rover, refreshImages: boolean = false) {
+    if (!cachedImageUrls)
+        cachedImageUrls = {
             curiosity: [],
             perseverance: [],
             spirit: []
         };
 
     // Pick random photo
-    if (refreshImages || cachedImages[rover].length === 0)
+    if (refreshImages || cachedImageUrls[rover].length === 0)
         await refreshCachedImages(rover);
 
-    const randomIndex = Math.floor(Math.random() * cachedImages[rover].length);
-    return cachedImages[rover][randomIndex];
+    const randomIndex = Math.floor(Math.random() * cachedImageUrls[rover].length);
+    return cachedImageUrls[rover][randomIndex];
 }
 
-async function refreshCachedImages(rover: Rover)
-{
+async function refreshCachedImages(rover: Rover) {
     const manifest = await GetRoverManifest(rover);
 
-    const randomSol = Math.floor(Math.random() * manifest.max_sol);
+    let randomSol = Math.floor(Math.random() * manifest.max_sol);
 
-    const roverPhotos = await GetRoverPhotos(rover, randomSol, "FHAZ")
-    cachedImages[rover] = roverPhotos.map(photo => photo.img_src)
+    let roverPhotos = await GetRoverPhotos(rover, randomSol, "FHAZ");
+
+    while (roverPhotos.length === 0)
+    {
+        randomSol = Math.floor(Math.random() * manifest.max_sol);
+        roverPhotos = await GetRoverPhotos(rover, randomSol, "FHAZ");
+    }
+
+    cachedImageUrls[rover] = roverPhotos.map(photo => photo.img_src);
 }
